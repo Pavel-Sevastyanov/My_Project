737eeb34b93245c46c206a3caed6cb3bf94533ed
Pavel Sevastyanov <sevastyanovit27@mail.ru>
2025-02-08 10:52:47 +0400

    creating new models

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, DatePost, Book, Person, Group
 
 admin.site.register(Post)
+admin.site.register(DatePost)
+admin.site.register(Book)
+admin.site.register(Person)
+admin.site.register(Group)
--- posts/models.py
+++ posts/models.py
@@ -4,5 +4,30 @@ from django.db import models
 class Post(models.Model):
     text = models.TextField()
 
+    class Meta:
+        verbose_name_plural = 'Текстовые данные'
+
     def __str__(self):
         return self.text[:50]
+
+
+class DatePost(models.Model):
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.date
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=20, help_text='Enter name book')
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    slug  = models.SlugField(max_length=255, unique=True, verbose_name='URL имя')
+    description = models.TextField(verbose_name='Описание')
